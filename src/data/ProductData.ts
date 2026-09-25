939bde2827c0daad621567c847552b18771c7a98
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-25 10:30:32 +0900

    feat: Implement ProductsData

---
+++ src/data/ProductData.ts
@@ -0,0 +1,9 @@
+import type { ProductType } from "../types/ProductType";
+
+const ProductsData: ProductType[] = [
+  { id: 1, title: "PC", price: 130000 },
+  { id: 2, title: "Tablet", price: 52000 },
+  { id: 3, title: "SmartPhone", price: 40000 },
+];
+
+export default ProductsData;
